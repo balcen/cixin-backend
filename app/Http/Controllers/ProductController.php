8725e5baeea31852aa1014ea46a12f0707e12dfc
baicen <jameslu8044@gmail.com>
2021-11-15 23:33:21 +0800

    add ProductController

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $productsQuery = Product::query();
+        if ($request->exists('product_category_ids')) {
+            $productsQuery->whereIn('product_category_id', $request->input('product_category_ids'));
+        }
+        $products = $productsQuery->orderBy('tracking_number')
+            ->get();
+
+        return $this->response
+            ->array(['products' => $products]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'tracking_number' => 'required',
+            'product_category_id' => 'required',
+            'name' => 'required',
+            'unit_id' => 'required',
+        ]);
+
+        $product = Product::query()
+            ->create($request->all());
+
+        return $this->response
+            ->array(['product' => $product->toArray()]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = Product::find($id);
+
+        return $this->response
+            ->array(['product' => $product->toArray()]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        Product::find($id)
+            ->update($request->except('tracking_number'));
+
+        return $this->response->created();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Product::find($id)
+            ->delete();
+
+        return $this->response->created();
+    }
+}
