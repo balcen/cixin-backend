0b7c3f338f4ddaecdcbae2eac4f8cb57485e75a2
baicen <jameslu8044@gmail.com>
2021-11-15 23:38:48 +0800

    add UnitController

---
+++ app/Http/Controllers/UnitController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Unit;
+use Illuminate\Http\Request;
+
+class UnitController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $units = Unit::all();
+
+        return $this->response
+            ->array(['units' => $units->toArray()]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'tracking_number' => 'required',
+            'name' => 'required',
+        ]);
+
+        $unit = Unit::query()
+            ->create($request->all());
+
+        return $this->response
+            ->array(['unit' => $unit->toArray()]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $unit = Unit::find($id);
+
+        return $this->response
+            ->array(['unit' => $unit->toArray()]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        Unit::find($id)
+            ->update($request->except('tracking_number'));
+
+        return $this->response->created();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Unit::find($id)
+            ->delete();
+
+        return $this->response->created();
+    }
+}
